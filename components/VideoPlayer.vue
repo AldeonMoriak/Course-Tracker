166f38a28121531f339c9c7912b77fe1dc892988
Mehran Mishekaran <aldeonmoriak@gmail.com>
2024-05-13 17:20:08 +0330

    test

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -2,7 +2,17 @@
   <ClientOnly>
     <div>
       <vue-plyr>
-        <div data-plyr-provider="youtube" :data-plyr-embed-id="videoId"></div>
+        <div data-plyr-provider="youtube" :data-plyr-embed-id="props.videoId"></div>
+      </vue-plyr>
+      <vue-plyr>
+        <div class="plyr__video-embed">
+          <iframe
+            src="https://www.youtube.com/embed/bTqVqk7FSmY?amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+            allowfullscreen
+            allowtransparency
+            allow="autoplay"
+          ></iframe>
+        </div>
       </vue-plyr>
     </div>
   </ClientOnly>
@@ -10,4 +20,5 @@
 
 <script setup lang="ts">
 const props = defineProps<{ videoId: string }>();
+console.log(props.videoId);
 </script>
