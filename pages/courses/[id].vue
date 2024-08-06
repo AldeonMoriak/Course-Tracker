0666240e15dee5c9ebb6e11c830209412b61248d
Mehran Mishekaran <aldeonmoriak@gmail.com>
2024-08-06 22:37:14 +0330

    fix bug

--- pages/courses/[id].vue
+++ pages/courses/[id].vue
@@ -2,14 +2,14 @@
   <div class="flex w-full justify-between">
     <div class="w-3/4">
       <div class="w-full">
-        <div :key="selectedVideo.id">
+        <div :key="selectedVideo?.id">
           <vue-plyr ref="plyr">
             <div class="plyr__video-embed">
               <iframe
                 :src="
-                  selectedVideo.source === 'youtube'
-                    ? `https://www.youtube.com/embed/${selectedVideo.video_id}?amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`
-                    : `https://player.vimeo.com/video/${selectedVideo.video_id}?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media`
+                  selectedVideo?.source === 'youtube'
+                    ? `https://www.youtube.com/embed/${selectedVideo?.video_id}?amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`
+                    : `https://player.vimeo.com/video/${selectedVideo?.video_id}?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media`
                 "
                 allowfullscreen
                 allowtransparency
@@ -20,11 +20,11 @@
         </div>
       </div>
       <div class="">
-        <div class="pb-5 text-2xl font-bold text-black">{{ selectedVideo.title }}</div>
+        <div class="pb-5 text-2xl font-bold text-black">{{ selectedVideo?.title }}</div>
         <Tags :tags="['1', '2']" />
       </div>
       <div class="">
-        <img :src="selectedVideo.thumbnail" alt="video thumbnail" />
+        <img :src="selectedVideo?.thumbnail" alt="video thumbnail" />
       </div>
     </div>
     <div class="h-screen w-1/4 bg-gray-100 p-4">
