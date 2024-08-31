8c5beb578e10064e06c56ea65d74b0ca0f693391
Mehran Mishekaran <aldeonmoriak@gmail.com>
2024-08-31 16:45:39 +0330

    add vueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxtjs/supabase', '@nuxt/image', '@nuxt/ui', '@vueuse/nuxt'],
   ui: {
     global: true,
   },
